b4a5b3d7e2f8a3abc706bb20d8e230b572b9bba4
gregorioospina <g.ospinaa@uniandes.edu.co>
2019-09-02 07:22:04 -0500

    countdown

---
+++ countdown/scripts.js
@@ -0,0 +1,31 @@
+/** code by webdevtrick ( https://webdevtrick.com ) **/
+const year = new Date().getFullYear();
+const chosenDate = new Date(2019, 8, 30).getTime();
+
+let countdown = setInterval(function() {
+
+  const today = new Date().getTime();
+  
+  const diff = chosenDate - today;
+  console.log(diff);
+  console.log(chosenDate);
+
+  
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    "<div class=\"days\"> \
+  <div class=\"c-number\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"c-number\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"c-number\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"c-number\">" + seconds + "</div>seconds</div> \
+<div class=\"left\">LEFT</div>\
+</div>";
+
+}, 1000);
